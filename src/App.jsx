9f9c089ab383ef1ef8162ff341ea751c62596fa5
Tristan Gruener <tristan.gruener@gmail.com>
2024-12-29 14:32:51 -0800

    save

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import heroImage from './assets/hero-image.png'
+import './styles/main.scss';
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <header>header</header>
+      <div class="hero">hero</div>
+      <div class="three-col-container">test test test</div>
+    </div>
+  )
+}
+
+export default App
